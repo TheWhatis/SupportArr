16fef99718c5fce51c445600b2d82d557d30dc1c
TheWhatis <anton-gogo@mail.ru>
2023-10-27 01:49:02 +0700

    Создал пакет

---
+++ README.md
@@ -0,0 +1,52 @@
+# Классы-помощники для работы с массивами
+## Документация
+  * [Documentation](https://github.com/TheWhatis/Settings/tree/master/docs/markdown/index.md "Documentation")
+## Установка
+```
+composer require whatis/support-arr
+```
+## Использование
+```php
+<?php
+/// ... Подключение пакета (require_once 'vendor/autoload.php')
+
+
+use Whatis\Support\Arr;
+
+$array = [
+    'key' => 'value',
+    'array' => [
+        'key' => 'value',
+        'entryarray' => [
+            'key' => 'value'
+        ]
+    ]
+];
+
+// Получение значений
+echo Arr::get($array, 'key'); // > value
+echo Arr::get($array, 'array.key'); // > value
+
+var_dump(Arr::get($array, 'array));
+// > [
+// >    'key' => 'value',
+// >    'entryarray' => [
+// >        'key' => 'value'
+// >    ]
+// > ]
+
+// Установка значений
+Arr::set($array, 'array.key1', 'myvalue');
+var_dump($array);
+// > [
+// >     'key' => 'value',
+// >     'array' => [
+// >         'key' => 'value',
+// >         'key1' => 'myvalue'
+// >         'entryarray' => [
+// >             'key' => 'value'
+// >         ]
+// >     ]
+// > ]
+// ... И т.д.
+```
---
+++ src/Arr.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Файл с вспомогательным
+ * классом для взаимодействия
+ * с массивами
+ *
+ * PHP version 8
+ *
+ * @category Support
+ * @package  Settings
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/Settings
+ */
+
+namespace Whatis\Settings\Support;
+
+use ArrayAccess;
+
+/**
+ * Вспомогательный класс массивов
+ *
+ * Вспомогательный класс
+ * для работы с массивами
+ *
+ * PHP version 8
+ *
+ * @category Support
+ * @package  Settings
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/Settings
+ */
+class Arr
+{
+    /**
+     * Проверка что значение сопоставляемое массиву
+     *
+     * @param mixed $value Значение
+     *
+     * @return bool
+     */
+    public static function accessible(mixed $value): bool
+    {
+        return is_array($value) || $value instanceof ArrayAccess;
+    }
+
+    /**
+     * Проверить что ключ есть в массиве
+     *
+     * @param string|int        $key   Ключ
+     * @param array|ArrayAccess $array Массив
+     *
+     * @return bool
+     */
+    public static function exists(
+        string|int $key,
+        array|ArrayAccess $array
+    ): bool {
+        if (is_array($array)) {
+            return array_key_exists($key, $array);
+        }
+
+        return $array->offsetExists($key);
+    }
+
+    /**
+     * Создать dotted массив
+     *
+     * Преобразовывает массив в
+     * одномерный, в котором
+     * вложенность переданного
+     * массива определяется
+     * ключами в одномерном
+     *
+     * Каждый отдельный ключ -
+     * это ключи, разделенные
+     * точками, определяющие
+     * вложенность переданного
+     * массива, пример: `path.to.key`
+     *
+     * @param iterable $array  Итерируемый объект
+     * @param string   $prefix Профикс (необходим
+     *                         для генерации)
+     *
+     * @return array
+     */
+    public static function dot(
+        iterable $array,
+        string $prefix = ''
+    ): array {
+        $result = [];
+        foreach($array as $key => $value) {
+            // Если массив то перебираем
+            // внутренние элементы с
+            // помощью static::dot
+            if(static::accessible($value)) {
+                $result = array_merge(
+                    $result, static::dot(
+                        $value, $prefix . $key . '.'
+                    )
+                );
+
+                // И пропусаем добавление
+                // значения для родителя
+                continue;
+            }
+
+            // Добавляем значение в массив
+            $result[$prefix . $key] = $value;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Развернуть dotted массив
+     *
+     * Разворачивает dotted массив
+     * (см. метод {@see Arr::dot()})
+     *
+     * @param array $array Массив
+     *
+     * @return array
+     */
+    public static function undot(array $array): array
+    {
+        $result = [];
+        foreach ($array as $key => $value) {
+            static::set($result, $key, $value);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Установить значение в массив
+     *
+     * Установить значение в массив
+     * по ключу (вложенные ключи,
+     * разделенные точками)
+     *
+     * @param array      $array Массив
+     * @param string|int $key   Ключ
+     * @param mixed      $value Значение
+     *
+     * @return array
+     */
+    public static function set(
+        array &$array,
+        string|int $key,
+        mixed $value
+    ): array {
+        foreach (($keys = explode('.', $key)) as $index => $key) {
+            // Если последний элемент, то
+            // устанавливем соответствующее
+            // значение в конец
+            if ($index === array_key_last($keys)) {
+                $array[$key] = $value;
+                break;
+            }
+
+            $array = &$array[$key];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Получить значение из массива
+     * по ключу
+     *
+     * Можно прописать путь через точку:
+     * `path.to.key`
+     *
+     * @param array|ArrayAccess $array   Массив
+     * @param string|int        $key     Ключ
+     * @param mixed             $default Значение по-умолчанию
+     *
+     * @return mixed
+     */
+    public static function get(
+        array|ArrayAccess $array,
+        string|int $key,
+        mixed $default = null
+    ): mixed {
+        // Если передан внутренний ключ
+        if (strpos($key, '.') === false) {
+            // Если ключ в массиве есть, то
+            // возвращаем его, иначе значение
+            // по-умолчанию
+            return $array[$key] ?? $default;
+        }
+
+        // Ищем значение в массиве
+        foreach (explode('.', $key) as $segment) {
+            if (static::accessible($array) && static::exists(
+                $segment, $array
+            )
+            ) {
+                $array = $array[$segment];
+                continue;
+            }
+
+            return $default;
+        }
+
+        // Возвращаем значение
+        return $array;
+    }
+}
